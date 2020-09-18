d84c83f0bd9929a143f636a334131a02175f3259
Nina Markovic <mnina5580@gmail.com>
2020-09-18 17:08:22 +0200

    Inicijalni komit,dodati jarovi i paketi

---
+++ IB_Project_Shell/src/main/java/ib/project/model/User.java
@@ -0,0 +1,156 @@
+package ib.project.model;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="USERS")
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = -1510940301878197333L;
+
+	@Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "certificate")
+    private String certificate;
+    
+    @Column(name = "active")
+    private Boolean active;
+    
+    @Column(name = "last_password_reset_date")
+    private Timestamp lastPasswordResetDate;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities;
+
+    public User() {
+
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	 public void setPassword(String password) {
+	        Timestamp now = new Timestamp(new Date().getTime());
+	        this.setLastPasswordResetDate(now);
+	        this.password = password;
+	    }
+
+	public String getCertificate() {
+		return certificate;
+	}
+
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+    
+    public Timestamp getLastPasswordResetDate() {
+        return lastPasswordResetDate;
+    }
+
+    public void setLastPasswordResetDate(Timestamp lastPasswordResetDate) {
+        this.lastPasswordResetDate = lastPasswordResetDate;
+    }
+    
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @JsonIgnore
+    public String getAuthoritiesAsString() {
+    	StringBuilder sb = new StringBuilder();
+    	
+    	for (Authority authority : this.authorities) {
+    		sb.append(authority.getName() + " ");
+    	}
+    	
+    	return sb.toString();
+    }
+
+	@Override
+	public boolean isEnabled() {
+		return active;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+}
